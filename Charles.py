a7fc38837ca8c4aa1ff6a0f276958edb65a82ee5
Charles Winkler <charleswinkler76@gmail.com>
2023-03-05 15:15:02 -0300

    tarefa

---
+++ Charles.py
@@ -0,0 +1,12 @@
+nome = "Charles"
+sobrenome = " David Winkler"
+idade = 19
+altura = "1.70"
+peso = " 70kg"
+
+print("\nMeu nome é " + nome + sobrenome + ",\ntenho " + str(idade) + " anos, tenho " + altura + " de altura e"+ peso + " de peso.")
+
+if idade >= 18:
+    print("\nParabéns! Você é maior de idade.")
+else:
+    print("Desculpe, você não é maior de idade.")
