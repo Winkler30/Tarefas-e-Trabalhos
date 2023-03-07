1bef9753f9b3aff6f502bf9fdc209896f3f38284
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-03-06 23:37:18 -0300

    PYTHON-06-03

---
+++ Atv-3.py
@@ -0,0 +1,9 @@
+numero1 = input("Digite um numero: ")
+numero2 = input("Digite outro numero: ")
+
+if numero1 > numero2:  
+    print ("O primeiro numero é maior")
+elif numero1 < numero2:
+    print ("O segundo numero é maior")
+else:
+    print ("Os numeros são iguais")
