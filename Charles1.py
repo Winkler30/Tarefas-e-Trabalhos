844c53779a085f3ba7be702c33a70cccc9fbac3f
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-03-29 23:39:31 -0300

    PYTHON 06-03

--- Charles1.py
+++ Charles1.py
@@ -1,17 +1,20 @@
-nome = input ("\n Qual o seu Nome ? ")
-altura = float(input(' Digite sua Altura: '))
-peso = float(input(' Digite seu Peso: '))
+nome = input("\nQual o seu Nome? ")
+altura = float(input('Digite sua Altura: '))
+peso = float(input('Digite seu Peso: '))
 
 print(f"\nSeu nome é: {nome}")
 
-divisao = peso / (altura * altura)
-print('Resultado da Divisão:', divisao)
+imc = peso / (altura * altura)
+print('Resultado da Divisão:', imc)
 
-if peso <= 18.5:
+if imc <= 18.5:
     print("\nSeu IMC é ABAIXO DO PESO.\n")
 
-if peso >= 18.5 and 24.9:
+elif imc >= 18.5 and imc <= 24.9:
     print("\nSeu IMC é NORMAL.\n")    
 
-elif peso <= 25 and 29.9:
-    print("Seu IMC é SOBREPESO\n.")
+elif imc >= 25 and imc <= 29.9:
+    print("Seu IMC é SOBREPESO.\n")
+
+else:
+    print("Seu IMC é OBESIDADE.\n")
